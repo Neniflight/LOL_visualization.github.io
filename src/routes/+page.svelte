7e82a02d56bd40c3faead7aa58541a681b69cc39
Calvin Nguyen <can005@ucsd.edu>
2024-02-19 14:24:14 -0800

    world map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+  import { json } from "d3";
+  import { geoPath, geoNaturalEarth1 } from "d3";
+  import { onMount } from 'svelte';
+
+  let dataset = [];
+  json(
+    "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
+  ).then((data) => {
+    dataset = data.features;
+  });
+  const width = 1200;
+  const height = 600;
+
+  const projection = geoNaturalEarth1();
+  const path = geoPath(projection);
+</script>
+
+<main>
+  <svg {width} {height}> 
+    {#each dataset as data}
+    <path
+      d={path(data)}
+    />
+    {/each}
+  </svg>
+</main>
